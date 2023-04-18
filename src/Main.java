a4e078f115d62f27a11136c305d866cad9ead78e
vladk <kotolupenko.vlad@mail.ru>
2023-04-18 16:32:22 +0500

    сделал дз

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("  Привет !");
+        System.out.println("  Привет м !");
 
 
         Author Griboyedov = new Author(" Alexander  ", " Griboyedov ");
